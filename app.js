3974bf331f24a7c2993dd223b8c60797c2964622
Kasper Doe <kasper@doe.dk>
2026-01-27 09:34:46 +0100

    feat(database): set up database connection with Sequelize
    Added configuration for database connection using Sequelize and dotenv. Created .env.example for environment variables and established a connection to a MariaDB database. Updated package.json to include necessary dependencies for Sequelize and dotenv.

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ app.set('view engine', 'ejs');
 
 app.use("/admin", (req, res) => {
    res.render('admin');
-   res.end();
 });
 
 app.use("/book", (req, res) => {
     res.render('book');
-    res.end();
 });
 
 app.listen(3000, (err) => console.log('Server is running on port 3000'));
