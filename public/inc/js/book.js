36a51eb48f6fb3f0ff63aaa42b213df3e7644ee2
Nikolaj F. larsen <nikolajfischer38@gmail.com>
2026-01-28 12:45:51 +0100

    WIP

--- public/inc/js/book.js
+++ public/inc/js/book.js
@@ -1,15 +1,59 @@
+console.log("Book page loaded");
 
-
-let weekJson = {
-    "week number": "3",
-    "weeks days": [
-        {"01-02": ["07:30"]},
-        {"02-03": ["07:30"]},
-        {"03-04": ["07:30"]},
+const weekJson = {
+    weekNumber: 3,
+    days: [
+        {
+            date: "2026-02-01",
+            label: "01/02",
+            availableTimes: ["07:30", "08:00"]
+        },
+        {
+            date: "2026-02-02",
+            label: "02/02",
+            availableTimes: ["07:30"]
+        },
+        {
+            date: "2026-02-03",
+            label: "03/02",
+            availableTimes: ["07:30"]
+        },
+        {
+            date: "2026-02-04",
+            label: "04/02",
+            availableTimes: []
+        },
+        {
+            date: "2026-02-05",
+            label: "05/02",
+            availableTimes: []
+        }
     ]
 }
 
-function getWeek(){
+getWeek()
 
+function getWeek(){
+    let head = document.getElementById("week-list");
+    let body = document.getElementById("book-list");
+
+    head.innerText = ''
+    let thRow = document.createElement("th")
+    thRow.innerText = "Week" + weekJson.weekNumber
+    head.appendChild(thRow)
+
+    body.innerHTML = ''
+
+    weekJson.days.forEach(day => {
+        let tr = document.createElement("tr");
+        tr.innerHTML = `<td>${day.label}</td>`;
+        console.log(day.availableTimes.length);
+        for (let i = 0; i < day.availableTimes.length; i++) {
+            let td = document.createElement("td");
+            td.innerText = day.availableTimes[i];
+            tr.appendChild(td);
+        }
+        body.appendChild(tr);
+    })
 
 }
